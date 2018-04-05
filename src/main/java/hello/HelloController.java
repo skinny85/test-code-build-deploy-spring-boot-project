495fba11ea879d03e5d614fe3993ca596f820e33
Adam Ruka <adamruka@amazon.com>
2018-04-05 14:55:53 -0700

    Change the controller to show the current date and time, to be more interesting.

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -3,10 +3,14 @@ package hello;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.time.Instant;
+import java.time.ZoneId;
+
 @RestController
 public class HelloController {
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        return String.format("Greetings from Spring Boot! The current date-time is: '%s'.",
+                Instant.now().atZone(ZoneId.of("America/Los_Angeles")).toString());
     }
 }
